1ca0edd51a3793f581b5b2008f6efff633db3657
Sahilpreet Singh <147434046+sps1001@users.noreply.github.com>
2024-11-06 20:06:31 +0530

    SSH(Public/Private keys added)

--- backend/Algo/wallet/Transaction.h
+++ backend/Algo/wallet/Transaction.h
@@ -5,20 +5,22 @@
 
 class Transaction {
 private:
-    std::string sender;
-    std::string receiver;
-    float amount;
-    std::string signature;
+std::string sender;
+std::string receiver;
+float amount;
+std::string signature;
 
 public:
-    Transaction() = default; // Default constructor
-    Transaction(const std::string& sender, const std::string& receiver, float amount);
+Transaction() = default; // Default constructor
+Transaction(const std::string& sender, const std::string& receiver, float amount);
 
-    std::string getData() const; // Get data to sign
-    std::string getSender() const;
-    std::string getReceiver() const;
-    float getAmount() const;
-    void setSignature(const std::string& sig);
+std::string getData() const; // Get data to sign
+std::string getSender() const;
+std::string getReceiver() const;
+float getAmount() const;
+void setSignature(const std::string& sig);
+bool signTransaction(const std::string& privateKey);
+bool verifySignature(const std::string& publicKey) const;
 };
 
 #endif // TRANSACTION_H
