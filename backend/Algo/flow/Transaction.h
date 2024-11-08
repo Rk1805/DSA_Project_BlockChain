179c9fa95fa56e54e4f493df1abaa92f93688c27
Sahil-1918912 <sahilsn104@gmail.com>
2024-11-08 14:56:40 +0530

    transaction file update

--- backend/Algo/flow/Transaction.h
+++ backend/Algo/flow/Transaction.h
@@ -9,9 +9,9 @@ public:
     std::string receiver;
     double amount;
 
-    Transaction(std::string sender, std::string receiver, double amount) 
+    Transaction(std::string sender, std::string receiver, double amount)
         : sender(sender), receiver(receiver), amount(amount) {}
-    
+
     std::string toString() const {
         return sender + "->" + receiver + ": " + std::to_string(amount);
     }
