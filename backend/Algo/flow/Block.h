833731628d819f244c5b26133e2a20a8eef84731
saherdev017 <166862259+saherdev017@users.noreply.github.com>
2024-11-10 18:07:32 +0530

    Updated Block.h

--- backend/Algo/flow/Block.h
+++ backend/Algo/flow/Block.h
@@ -7,34 +7,45 @@
 #include "MerkleTree.h"
 #include "ProofOfWork.h"
 
+using namespace std;
+
 class Block {
 public:
-    std::string previousBlockHash;
-    std::string merkleRoot;
-    int nonce;
-    std::string hash;
-    std::vector<Transaction> transactions;
+    string previousBlockHash; //Stores the hash of the previous block, linking this block to the previous one.
+    string merkleRoot;//Holds the Merkle root hash of all transactions in this blo
+    int nonce;//Used in proof-of-work to create a valid block hash.
+    string hash//;Stores the hash of this block.
+    vector<Transaction> transactions;//A vector containing the transactions for this block.
 
-    Block(const std::vector<Transaction>& transactions, const std::string& prevHash)
+    /* CONTSTRUCTOR*/
+    Block(const vector<Transaction>& transactions, const string& prevHash)
         : previousBlockHash(prevHash), transactions(transactions), nonce(0), hash("") {
-        MerkleTree merkleTree;
+        MerkleTree merkleTree;//creates a Merkle tree object.
         merkleRoot = merkleTree.calculateMerkleRoot(getTransactionHashes());
+        // is set by calculating the Merkle root hash from all transaction hashes using calculateMerkleRoot.
     }
 
-    std::string calculateBlockHash() const {
-        return sha256(previousBlockHash + merkleRoot + std::to_string(nonce));
+    string calculateBlockHash() const {
+        return sha256(previousBlockHash + merkleRoot + to_string(nonce));
     }
 
-    void setNonce(int newNonce) { nonce = newNonce; }
-    void setHash(const std::string& newHash) { hash = newHash; }
+    void setNonce(int newNonce) { nonce = newNonce; }//setting the nonce 
+/* basically nonce is a type of iterator for guess to assume how many guesses it took to solve the pow puzzle */
+    void setHash(const string& newHash) { hash = newHash; }
 
-    std::string getData() const {
+    string getData() const {//will be used in proof of work to solve the puzzle
         return previousBlockHash + merkleRoot;
     }
 
 private:
-    std::vector<std::string> getTransactionHashes() const {
-        std::vector<std::string> hashes;
+/*    
+This  function returns a vector of SHA-256 hashes for each transaction in the block:
+It iterates over transactions.
+For each transaction, it calls transaction.toString() to get a string representation and then hashes it with sha256.
+The resulting list of hashes is returned.
+*/
+    vector<string> getTransactionHashes() const {//to get the transaction hashes to be used in merkle tree
+        vector<string> hashes;
         for (const auto& transaction : transactions) {
             hashes.push_back(sha256(transaction.toString()));
         }
